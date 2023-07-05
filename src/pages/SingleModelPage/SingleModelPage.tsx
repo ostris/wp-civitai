728e9d0c14ef8838fafb4a5eba01fe4e7c955830
Jaret Burkett <jaretburkett@gmail.com>
2023-07-04 22:22:05 -0600

    Added base image carousel

--- src/pages/SingleModelPage/SingleModelPage.tsx
+++ src/pages/SingleModelPage/SingleModelPage.tsx
@@ -1,14 +1,65 @@
 import React from 'react';
 import getComponentData from '../../utils/getComponentData';
 import Model from '../../interfaces/Model';
+import { Carousel } from '@mantine/carousel';
 
-const modelData = getComponentData('SingleModelPage') as Model | null;
+const model = getComponentData('SingleModelPage') as Model | null;
 
 const SingleModelPage = () => {
-  if (!modelData) return null;
+  const [selectedVersion, setSelectedVersion] = React.useState(0);
+  // const { columnCount, columnWidth, maxSingleColumnWidth } = useMasonryContainerContext();
+  if (!model) return null;
+  const { columnCount, columnWidth, maxSingleColumnWidth } = {
+    columnCount: 3,
+    columnWidth: null,
+    maxSingleColumnWidth: '33%',
+  };
+  const totalItems = 1;
+  const version = model.modelVersions[selectedVersion];
+  const { data, itemId, height, extra } = {
+    data: version.images,
+    itemId: (item: any) => item.hash,
+    height: 600,
+    extra: null,
+  };
+
   return (
     <>
-      <div dangerouslySetInnerHTML={{ __html: modelData.description }}></div>
+      <Carousel
+        key={model.id}
+        // classNames={classes}
+        slideSize={`${100 / columnCount}%`}
+        slideGap="md"
+        align={totalItems <= columnCount ? 'start' : 'end'}
+        withControls={totalItems > columnCount ? true : false}
+        slidesToScroll={columnCount}
+        // height={columnCount === 1 ? maxSingleColumnWidth : '100%'}
+        loop
+        sx={{
+          width: columnCount === 1 ? maxSingleColumnWidth : '100%',
+          maxWidth: '100%',
+          margin: '0 auto',
+          minHeight: 600,
+        }}
+      >
+        {data.map((item, index) => {
+          const key = itemId ? itemId(item) : index;
+          return (
+            <Carousel.Slide key={key} id={key.toString()}>
+              <div style={{ position: 'relative', paddingTop: '100%', height: height }}>
+                {/*{createRenderElement(RenderComponent, index, item, height)}*/}
+                <img src={item.url} alt={model.name} style={{ position: 'absolute', top: 0, left: 0 }} />
+              </div>
+            </Carousel.Slide>
+          );
+        })}
+        {extra && (
+          <Carousel.Slide>
+            <div style={{ position: 'relative', paddingTop: '100%' }}>{extra}</div>
+          </Carousel.Slide>
+        )}
+      </Carousel>
+      <div dangerouslySetInnerHTML={{ __html: model.description }}></div>
     </>
   );
 };
