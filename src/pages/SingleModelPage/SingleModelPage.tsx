3f5fbdab1d06a6d8eb9fbeab5665470a05c38a6a
Jaret Burkett <jaretburkett@gmail.com>
2023-07-04 22:40:24 -0600

    adjustd col size

--- src/pages/SingleModelPage/SingleModelPage.tsx
+++ src/pages/SingleModelPage/SingleModelPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import getComponentData from '../../utils/getComponentData';
 import Model from '../../interfaces/Model';
 import { Carousel } from '@mantine/carousel';
+import { Grid } from '@mantine/core';
 
 const model = getComponentData('SingleModelPage') as Model | null;
 
@@ -10,11 +11,10 @@ const SingleModelPage = () => {
   // const { columnCount, columnWidth, maxSingleColumnWidth } = useMasonryContainerContext();
   if (!model) return null;
   const { columnCount, columnWidth, maxSingleColumnWidth } = {
-    columnCount: 3,
+    columnCount: 2,
     columnWidth: null,
     maxSingleColumnWidth: '33%',
   };
-  const totalItems = 1;
   const version = model.modelVersions[selectedVersion];
   const { data, itemId, height, extra } = {
     data: version.images,
@@ -22,43 +22,53 @@ const SingleModelPage = () => {
     height: 600,
     extra: null,
   };
+  const totalItems = data.length;
 
   return (
     <>
-      <Carousel
-        key={model.id}
-        // classNames={classes}
-        slideSize={`${100 / columnCount}%`}
-        slideGap="md"
-        align={totalItems <= columnCount ? 'start' : 'end'}
-        withControls={totalItems > columnCount ? true : false}
-        slidesToScroll={columnCount}
-        // height={columnCount === 1 ? maxSingleColumnWidth : '100%'}
-        loop
-        sx={{
-          width: columnCount === 1 ? maxSingleColumnWidth : '100%',
-          maxWidth: '100%',
-          margin: '0 auto',
-          minHeight: 600,
-        }}
-      >
-        {data.map((item, index) => {
-          const key = itemId ? itemId(item) : index;
-          return (
-            <Carousel.Slide key={key} id={key.toString()}>
-              <div style={{ position: 'relative', paddingTop: '100%', height: height }}>
-                {/*{createRenderElement(RenderComponent, index, item, height)}*/}
-                <img src={item.url} alt={model.name} style={{ position: 'absolute', top: 0, left: 0 }} />
-              </div>
-            </Carousel.Slide>
-          );
-        })}
-        {extra && (
-          <Carousel.Slide>
-            <div style={{ position: 'relative', paddingTop: '100%' }}>{extra}</div>
-          </Carousel.Slide>
-        )}
-      </Carousel>
+      <Grid>
+        <Grid.Col span={8}>
+          <Carousel
+            key={model.id}
+            // classNames={classes}
+            slideSize={`${100 / columnCount}%`}
+            slideGap="md"
+            align={totalItems <= columnCount ? 'start' : 'end'}
+            withControls={totalItems > columnCount ? true : false}
+            slidesToScroll={columnCount}
+            // height={columnCount === 1 ? maxSingleColumnWidth : '100%'}
+            loop
+            sx={{
+              width: columnCount === 1 ? maxSingleColumnWidth : '100%',
+              maxWidth: '100%',
+              margin: '0 auto',
+              minHeight: 600,
+            }}
+          >
+            {data.map((item, index) => {
+              const key = itemId ? itemId(item) : index;
+              return (
+                <Carousel.Slide key={key} id={key.toString()}>
+                  <div style={{ position: 'relative', paddingTop: '100%', height: height }}>
+                    {/*{createRenderElement(RenderComponent, index, item, height)}*/}
+                    <img src={item.url} alt={model.name} style={{ position: 'absolute', top: 0, left: 0 }} />
+                  </div>
+                </Carousel.Slide>
+              );
+            })}
+            {extra && (
+              <Carousel.Slide>
+                <div style={{ position: 'relative', paddingTop: '100%' }}>{extra}</div>
+              </Carousel.Slide>
+            )}
+          </Carousel>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <h1>{model.name}</h1>
+          <h2>{version.name}</h2>
+          <p>{version.description}</p>
+        </Grid.Col>
+      </Grid>
       <div dangerouslySetInnerHTML={{ __html: model.description }}></div>
     </>
   );
